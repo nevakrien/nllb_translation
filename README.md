57d9abde70f5f8669ea328d1e04e15c647c604bd
User <nevo.krien@gmail.com>
2024-02-06 23:49:05 +0200

    yay it works!!!

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nllb_translation
+simple translation package for python supports quntized infrence on gpu and regular infrence on cpu. 
+regular gpu infrence seems unpractical at least for hebrew so I am keeping the code there but not devloping it further
+
+
+
 getting the best I can from the translation model using some extra tricks
+seems 4-bit works perfectly fine so we use that on gpu and then we can get a lot of beams going and its really good.
--- translate.py
+++ translate.py
@@ -1,3 +1,7 @@
+# import os
+
+# # Make CUDA devices invisible to PyTorch
+# os.environ['CUDA_VISIBLE_DEVICES'] = ''
 from transformers import AutoModelForSeq2SeqLM, NllbTokenizerFast
 import torch
 
@@ -11,7 +15,25 @@ import gc
 SEP_TOKEN=2 #specific to nllb I just picked it up
 
 @torch.no_grad()
-def _translate_text_chunk(text,tgt_text,tokenizer,model,max_new_tokens=2000,gpu_len=1000):
+def _translate_text_chunk(text,tgt_text,tokenizer,model,max_new_tokens=2000,num_beams=3):
+    # Tokenize and translate the text
+    encoded_text = tokenizer(text, return_tensors="pt")
+    #manual fix to hf bug 
+    encoded_text['input_ids'][:,1]=tokenizer.lang_code_to_id[tokenizer.src_lang]
+
+    tgt_tokens=tokenizer.encode(tgt_text,add_special_tokens=False)
+    tgt_tokens=torch.LongTensor([[SEP_TOKEN,tokenizer.lang_code_to_id[tokenizer.tgt_lang]]+tgt_tokens]).to(model.device)
+
+    encoded_text={k:v.to(model.device) for k,v in encoded_text.items()}
+
+    generated_tokens=model.generate(**encoded_text, decoder_input_ids=tgt_tokens,max_new_tokens=max_new_tokens,
+            penalty_alpha=0.4,num_beams=num_beams).cpu()
+
+    return tokenizer.decode(generated_tokens[0][tgt_tokens.shape[1]:], skip_special_tokens=True)
+
+
+@torch.no_grad()
+def _translate_text_chunk_failsafe(text,tgt_text,tokenizer,model,max_new_tokens=2000,gpu_len=1000):
     # Tokenize and translate the text
     encoded_text = tokenizer(text, return_tensors="pt")
     #manual fix to hf bug 
@@ -61,20 +83,32 @@ def get_model_and_tokenizer(tgt_lang="heb_Hebr",src_lang="eng_Latn",cuda=True):
     model_name="facebook/nllb-200-3.3B"
     # Initialize the tokenizer and model
     tokenizer = NllbTokenizerFast.from_pretrained(model_name,tgt_lang=tgt_lang,src_lang=src_lang)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)#,load_in_4bit=True)
+    #print(model.device)
     
     if(cuda):
         model.to('cuda')
 
     return model,tokenizer
 
-def translate_text(text,tokenizer,model):
+#gpu only sadly... mainly used for the testing enviorment
+def get_quantmodel_and_tokenizer(tgt_lang="heb_Hebr",src_lang="eng_Latn"):
+    model_name="facebook/nllb-200-3.3B"
+    # Initialize the tokenizer and model
+    tokenizer = NllbTokenizerFast.from_pretrained(model_name,tgt_lang=tgt_lang,src_lang=src_lang)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name,load_in_4bit=True)
+    #model.to('cpu')
+    print(model.device)
+
+    return model,tokenizer
+
+def translate_text(text,tokenizer,model,num_beams=3):
     ans=''
 
     prev=''
     for t in text.split('\n\n'):
         # try:
-        prev=_translate_text_chunk(t,prev,tokenizer,model)
+        prev=_translate_text_chunk(t,prev,tokenizer,model,num_beams=num_beams)
         # except torch.cuda.OutOfMemoryError:
         #     model.to('cpu')
         #     prev=_translate_text_chunk(t,prev,tokenizer,model)
@@ -114,7 +148,7 @@ if __name__=="__main__":
     #     print(text)  
     text=english_texts[0]
     
-    model,tokenizer=get_model_and_tokenizer()
+    model,tokenizer=get_quantmodel_and_tokenizer()#get_model_and_tokenizer()
     print(text)
     print(10*'\n')
     trans=translate_text(text,tokenizer,model)
